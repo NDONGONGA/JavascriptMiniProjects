776642c8bdd4920884470d0005e90656cf36705c
NDONGO NGA <ndongonga@outlook.com>
2025-12-01 01:58:17 +0100

    if statements

---
+++ ifStatements/index.js
@@ -0,0 +1,20 @@
+//If statements
+
+let age = 15 ;
+let hasLicense = false;
+
+if(age >= 16){
+    console.log("you are old enough to drive");
+
+    if(hasLicense){
+        console.log("you have your license");
+        
+    }
+    else{
+        console.log("You donot have your license yet");
+        
+    }
+}else{
+    console.log("you are too young to drive");
+    
+}
